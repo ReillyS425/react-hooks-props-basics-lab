904f2d60b3b76ca00235f2c8eca14f50932fdc22
Reilly Shofner <reilly.shofner@gmail.com>
2025-05-02 17:10:00 -0700

    complete react-hooks-props-basics-lab

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
-  bio: "I made this!",
+  name: "Reilly",
+  city: "Everett",
+  bio: "I am A Software Engineering student from Washington State, I have a pug named Alfred, and I will be moving to Silverdale WA with my Fiance soon!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/ReillyS425",
+    linkedin: "https://www.linkedin.com/in/reilly-shofner-2b9438363/",
   },
 };
 
